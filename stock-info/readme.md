35f3af2ec1ccae7691f5f8479fbfbf8048a18aa2
Hanzallah Burney <hanzallahazimburney@live.com>
2022-11-28 14:08:45 +0500

    Create reddit and db connections

--- stock-info/readme.md
+++ stock-info/readme.md
@@ -3,4 +3,4 @@ This script pulls information on the top stock tickers from the subreddit r/wall
 The stock information is put into a SQL database for future use.
 
 #### How to run?
-To be updated
+* `python3 stock_info.py`
---
+++ stock-info/stock_info.py
@@ -0,0 +1,73 @@
+
+import os
+import sqlite3
+import praw
+import yfinance as yf
+from dotenv import load_dotenv, find_dotenv
+
+class Stock:
+    def __init__(self, name, sector, price, symbol, revenue) -> None:
+        self.name = name
+        self.sector = sector
+        self.price = price
+        self.symbol = symbol
+        self.revenue = revenue
+
+class StockHandler:
+    def __init__(self) -> None:
+        load_dotenv()
+
+        reddit = praw.Reddit(
+            client_id=os.getenv('reddit_key'),
+            client_secret=os.getenv('reddit_secret'),
+            password=os.getenv('reddit_password'),
+            user_agent=os.getenv('reddit_agent'),
+            username=os.getenv('reddit_username'),
+        )
+
+        self.wsb_subreddit = reddit.subreddit('wallstreetbets')
+
+
+    def get_top_stocks(self, limit):
+        None
+    
+    def get_stock_information():
+        None
+    
+class DatabaseHandler:
+    def __init__(self, db_name, table_name) -> None:
+        self.db_conn = sqlite3.connect(db_name)
+        self.db_cursor = self.db_conn.cursor()
+        self.table_name = table_name
+
+    def db_save_info(self, stock: Stock):
+        self.db_cursor.execute(
+            """
+            INSERT INTO ?
+            (
+                Company_Name, Sector, Stock_Price, Symbol, Total_Revenue
+            )
+            VALUES (?, ?, ?, ?, ?);
+            """, (
+                stock.name, stock.sector, stock.price, stock.symbol, stock.revenue
+            )
+        )
+        self.db_conn.commit()
+
+    def db_create(self):
+        self.db_cursor.execute(
+            """"CREATE TABLE IF NOT EXISTS ?
+            (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                Company_Name TEXT,
+                Sector TEXT,
+                Stock_Price,
+                Symbol TEXT,
+                Total_Revenue INTEGER
+            );
+            """, (self.table_name)
+        )
+        self.db_conn.commit()
+
+if __name__ == '__main__':
+    StockHandler()
